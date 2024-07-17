29fed6ef2a3bfd117897012f824fa35224a85409
carlosgodspeed <carlosminibics@gmail.com>
2024-07-16 22:09:01 -0300

    .

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>teste</Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-xl">teste</Text>
     </View>
   );
 }
